765e32b02af37777014f71d80183ce4dfa639013
Chuchoncio <jmpch.111@gmail.com>
2022-03-18 00:09:54 -0300

    Second commit and i got excited so i already add Map methods besides Set methods

--- src/testingcollections/TestingCollections.java
+++ src/testingcollections/TestingCollections.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class TestingCollections {
     
@@ -65,7 +67,9 @@ public class TestingCollections {
         System.out.println("Lista desordenada");
         iterarSobreUnaLista(numerosList);
         Collections.sort(numerosList);
+        
         System.out.println("");
+        
         System.out.println("Lista ordenada");
         iterarSobreUnaLista(numerosList);
     }
@@ -78,15 +82,56 @@ public class TestingCollections {
         agregarDatosSet(numerosSet, 1);
         agregarDatosSet(numerosSet, 5);
         agregarDatosSet(numerosSet, 90);
-                
+        
+        System.out.println("Datos desordenados");
+        mostrarDatosSet(numerosSet);
+        
+        // Transforming HashSet into ArrayList because sort method doesn't
+        //  know how to orden by hash atribute.
+        ArrayList<Integer> numerosLista = new ArrayList(numerosSet);
+        
+        System.out.println("");
+        Collections.sort(numerosLista);
+        System.out.println("Datos ordenados");
+        mostrarDatosList(numerosLista);
+    }
+    
+    public static void mostrarDatosTreeMap(TreeMap<Integer, String> treeMap) {
+        System.out.println("Imprimiendo Tree Map");
+        for (Map.Entry<Integer, String> entry : treeMap.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            
+            System.out.println("Llave: " + key + " | Valor: " + value);
+        }
+    }
+    
+    public static void operacionesMapas() {
+        HashMap<Integer, String> alumnosMap = new HashMap();
+        
+        agregarDatosMap(alumnosMap, 3, "Chuchu");
+        agregarDatosMap(alumnosMap, 1, "Lucas");
+        agregarDatosMap(alumnosMap, 5, "Agus");
+        agregarDatosMap(alumnosMap, 30, "Lean");
+        agregarDatosMap(alumnosMap, 15, "Nahuel");
+        agregarDatosMap(alumnosMap, 11, "Gimena");
+        
+        System.out.println("Datos desordenados");
+        mostrarDatosMap(alumnosMap);
+        
+        // Transforming HashMap into TreeMap because sort method doesn't
+        //  know if order by key or by value
+        TreeMap<Integer, String> alumnosTree = new TreeMap(alumnosMap);
+        
+        System.out.println("");
+        System.out.println("Datos ordenados");
+        mostrarDatosTreeMap(alumnosTree);
     }
 
     public static void main(String[] args) {
-        operacionesListas();
-        operacionesConjuntos();
-        
-        HashMap<Integer, String> alumnosMap = new HashMap();
-        agregarDatosMap(alumnosMap, 39515920, "Chuchu");
+        //operacionesListas();
+        //operacionesConjuntos();
+        operacionesMapas();
     }
 
 }
